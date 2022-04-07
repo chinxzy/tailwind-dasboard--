5af1a44400d1cb2dccd18a81c6697d34fef6c270
chinxzy <chinxzypoet@gmail.com>
2022-04-07 10:17:05 +0100

    first initial commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { FontAwesomeIcon } from "./plugins/font-awesome";
+import "./index.css";
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
